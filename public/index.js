a51695e9d0920db53d07cbc0b402780b4770e91f
Oksuriini <oskari.peiponen@hotmail.com>
2023-03-03 19:00:17 +0200

    Fixed folder creation

--- public/index.js
+++ public/index.js
@@ -2,23 +2,30 @@ const express = require("express")
 const fs = require("fs")
 const app = express()
 app.use(express.json())
-const path = "./public/data/weatherdata.json"
-
+const pathOfFile = "./public/data/weatherdata.json"
+const pathOfDir = "./public/data"
 app.post("/weather", async (req, res) =>{
     const latitude = req.body.latitude
     const longitude = req.body.longitude
     if(!Number.isNaN(latitude) || !Number.isNaN(longitude)){
         const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,rain_sum,snowfall_sum,windspeed_10m_max,winddirection_10m_dominant&timezone=auto`)
         const data = await response.json()
+        fs.mkdir(pathOfDir, (err) =>{
+        if(err){
+            console.log("Creating data directory failed.")
+            console.log(err)
+        }
+    })
         await console.log(data)
-        await fs.writeFileSync(path, JSON.stringify(data))
+        await fs.writeFileSync(pathOfFile, JSON.stringify(data))
     } else {
         res.status(550).json("Sent data is not legal.")
     }
 })
 
 app.get("/data", async (req, res) => {
-    const data = await JSON.parse(fs.readFileSync("./public/data/weatherdata.json"))
+    
+    const data = await JSON.parse(fs.readFileSync(pathOfFile))
     res.json(data)
     console.log(data)
 })
