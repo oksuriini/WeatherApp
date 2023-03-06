692c13153a78ff1f7c35b7280489f96ae35a8704
Oksuriini <oskari.peiponen@hotmail.com>
2023-03-06 18:09:28 +0200

    added 7 day summary, and fixed some backend code

--- public/index.js
+++ public/index.js
@@ -4,18 +4,23 @@ const app = express()
 app.use(express.json())
 const pathOfFile = "./public/data/weatherdata.json"
 const pathOfDir = "./public/data"
+
 app.post("/weather", async (req, res) =>{
-    const latitude = req.body.latitude
-    const longitude = req.body.longitude
-    if(!Number.isNaN(latitude) || !Number.isNaN(longitude)){
-        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,rain_sum,snowfall_sum,windspeed_10m_max,winddirection_10m_dominant&timezone=auto`)
+    const latitude = parseFloat(req.body.latitude)
+    const longitude = parseFloat(req.body.longitude)
+    if(!Number.isNaN(latitude) && !Number.isNaN(longitude)){
+        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,rain_sum,snowfall_sum,windspeed_10m_max&timezone=auto`)
         const data = await response.json()
-        fs.mkdir(pathOfDir, (err) =>{
-        if(err){
-            console.log("Creating data directory failed.")
-            console.log(err)
+        if(!fs.existsSync(pathOfDir)){
+            console.log("Folder 'data' not found in path: './public', creating one...")
+            fs.mkdir(pathOfDir, (err) =>{
+                if(err){
+                    console.log("Creating data directory failed.")
+                    console.log(err)
+                }
+            })
+            console.log("Created folder 'data' in path: './public'")
         }
-    })
         await console.log(data)
         await fs.writeFileSync(pathOfFile, JSON.stringify(data))
     } else {
